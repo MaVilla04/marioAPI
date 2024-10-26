cbe883a79b9507e10360053158b8b5539705fcc1
MaVilla <melissa.villa2019@gmail.com>
2024-10-25 22:22:37 -0500

    agg ruta get

--- routes/mariosTrans.js
+++ routes/mariosTrans.js
@@ -1,7 +1,14 @@
-const rutaBase = "/mariosTransformations";
+const rutaBase = "/marioTransformations";
 
 exports.mario = (app, transformations) => {
-  app.get(`${rutaBase}`, (req, res) => {
-    res.json(transformations);
+  app.get(`${rutaBase}`, async (req, res) => {
+    try {
+      const transformations = await Transformation.find().sort({ _id: -1 }); //ordena por ID desde el más antiguo al más reciente
+      res.json(transformations);
+    } catch (error) {
+      res.status(500).json({ msg: "Error al obtener las transformaciones" });
+    }
   });
+
+  app.post = (`${rutaBase}`, async (req, res) => {});
 };
