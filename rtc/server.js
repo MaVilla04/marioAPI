5a45e15bd7689f5630bbe1e1bb639bb668947a5b
MaVilla <melissa.villa2019@gmail.com>
2024-11-30 20:29:40 -0500

    corrige cors del server.js

--- rtc/server.js
+++ rtc/server.js
@@ -4,6 +4,9 @@ exports.ioSocket = (server) => {
   const io = new Server(server, {
     cors: {
       origin: "*",
+      methods: ["GET", "POST"],
+      allowedHeaders: ["my-custom-header"],
+      credentials: true,
     },
   });
 
